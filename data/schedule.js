1f8deb534bf68560691259faaca63afac8e97c7d
songseunghyun <songsh96927@gmail.com>
2022-03-19 12:59:33 +0900

    CRUD로직분리(22.03.19)

--- data/schedule.js
+++ data/schedule.js
@@ -1,2 +1,74 @@
+var scheduleData = [
+    {
+        'id' : '1',
+        'date' : '20220319',
+        'title' : '회의',
+        'text' : '캡스톤 회의'
+    },
+    {
+        'id' : '2',
+        'date' : '20220320',
+        'title' : '운동',
+        'text' : '운동'
+    },
+    {
+        'id' : '3',
+        'date' : '20220320',
+        'title' : '여행',
+        'text' : '제주도 여행'
+    },
+]
+
+//get all
+export async function getAll(){
+    return scheduleData;
+}
+
 //get by date
-//get by particular schedule
+export async function getbyDate(date){
+    const schedule = scheduleData.filter(
+        (schedule) => schedule.date === date
+    );
+    
+    return schedule;
+}
+
+//get by id
+export async function getbyId(id){
+    const schedule = scheduleData.find(
+        (schedule) => schedule.id === id
+    );
+
+    return schedule;
+}
+
+//create schedule
+export async function create(date, title, text){
+    const schedule = {
+        id: Date.now().toString(),
+        date,
+        title,
+        text
+    };
+    scheduleData = [...scheduleData, schedule];
+    
+    return schedule.id;
+}
+
+//update schedule
+export async function update(id, title, text){
+    const schedule = await getbyId(id);
+
+    schedule.title = title;
+    schedule.text = text;
+
+    return schedule;
+}
+
+//delete schedule
+export async function remove(id){
+    scheduleData = scheduleData.filter(
+        (schedule) => schedule.id !== id
+    );
+    
+}
---
+++ middleware/validator.js
@@ -0,0 +1,2 @@
+import { validationResult } from "express-validator";
+
