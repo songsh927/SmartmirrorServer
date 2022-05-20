75645b028a16fe7c4df8808508021d40baef7283
songseunghyun <songsh96927@gmail.com>
2022-05-20 11:53:42 +0900

    외부모듈제어api수정(22.05.20)

--- controller/remote.js
+++ controller/remote.js
@@ -2,203 +2,86 @@ import request from 'request';
 import 'request-promise-native';
 import 'express-async-errors';
 
-// var lightStatus = {
-//     "ctrl": "off",
-//     "redValue": "",
-//     "greenValue": "",
-//     "blueValue": ""
-// }
-
-// var curtainStatus = {
-//     "ctrl" : "off",
-//     "openTime" : "",
-//     "closeTime" : ""
-// }
-
-// var tempStatus = {
-//     "ctrl" : "off",
-//     "onTime" : "",
-//     "offTime" : ""
-// }
-
-///
-var instStatus = [
-    {
-        "inst": "lightStatus",
-        "ctrl": "off",
-        "redValue": "",
-        "greenValue": "",
-        "blueValue": ""
-    },
-    {
-        "inst": "curtainStatus",
-        "ctrl" : "off",
-        "openTime" : "",
-        "closeTime" : ""
-    },
-    {
-        "inst": "tempStatus",
-        "ctrl" : "off",
-        "onTime" : "",
-        "offTime" : ""
-    }
-]
-///
-
-//TODO! 요청시 모듈의 상태 받아온후 처리
-/*
 // == light == //
 export async function lightGetStatus(req, res){
-    return res.status(200).json(lightStatus);
+    requestModuleGetStatus('lightcontroller').then((body) => {
+        res.status(200).json(body)
+    })
 }
 
 export async function lightControl(req, res){
-    const ctrl = req.query.ctrl;
-    const redValue = req.query.redValue;
-    const greenValue = req.query.greenValue;
-    const blueValue = req.query.blueValue; //조명 RGB 
-    if(lightStatus.ctrl == ctrl){
-        return res.status(200).json(lightStatus)
-    }
-    lightStatus.ctrl = ctrl;
-    return res.status(200).json(lightStatus);
-    
+    const {ctrl, redValue, greenValue, blueValue} = req.body;
+    requestModuleController('lightcontroller', {ctrl, redValue, greenValue, blueValue}).then((body) => {
+        res.status(200).json(body)
+    })
 }
+
 
 // == curtain== //
 export async function curtainGetStatus(req, res){
-    return res.status(200).json(curtainStatus);
+    requestModuleGetStatus('curtaincontroller').then((body) => {
+        res.status(200).json(body)
+    })
 }
+
 export async function curtainControl(req, res){
-    const ctrl = req.query.ctrl;
-    const onTime = req.query.onTime
-    const closeTime = req.query.offTime//커튼 여닫는 시간예약
-    if(curtainStatus.ctrl == ctrl){
-        return res.status(200).json(curtainStatus)
-    }
-    curtainStatus.ctrl = ctrl;
-    return res.status(200).json(curtainStatus);
+    const {ctrl, onTime, offTime} = req.body;
+    requestModuleController('curtaincontroller', {ctrl, onTime, offTime}).then((body) => {
+        res.status(200).json(body)
+    })
 }
 
 // == temp == // 
 export function tempGetStatus(req, res, next){
-    return res.status(200).json(tempStatus);
+    requestModuleGetStatus('tempcontroller').then((body) => {
+        res.status(200).json(body)
+    })
 }
+
 export async function tempControl(req, res){
-    const ctrl = req.query.ctrl;
-    const onTime = req.query.onTime
-    const closeTime = req.query.offTime //켜짐,꺼짐 예약
-    console.log(ctrl);
-    if(tempGetStatus == ctrl){
-        return res.status(200).json(tempStatus)
-    }
-    //requestApi.post
-    statusData.tempStatus = ctrl;
-    return res.status(200).json(tempStatus);
+    const {ctrl, onTime, offTime, temp} = req.body;
+    requestModuleController('tempcontroller', {ctrl, onTime, offTime, temp}).then((body) => {
+        res.status(200).json(body)
+    })
 }
 
 
 export async function getStatus(req, res){
     return res.status(200).json(statusData);
 }
-*/
-// test api //
 
 
-/*
-상태 확인(GET)요청
-요청형태
-http://192.168.0.X/remote/status/lightStatus
-*/
-export async function testGetStatus(req, res){
-    const inst = req.params.inst;
-    const uri = 'http://localhost:8080/remote/status/' + inst   //모듈쪽 주소로 변경
-    console.log(inst);
-    request(uri, function(err, res, body){
-        console.log(body);
-    })
-    res.sendStatus(200);
-}
-
-export async function testController(req, res){
-    const instId = req.params.inst;
-    const ctrl = req.query.ctrl;
-    const inst = instStatus.find((inst) => inst.inst === instId);
-    
-    //inst.ctrl = ctrl;
-    
-    
-    if(await requestModuleControll(inst.inst, inst) == true){
-        res.sendStatus(200);
-    }else{
-        console.error(err);
-        res.sendStatus(500);
-    }
-    
-    
-    
-}
-
-
-/*
-상태 변경(POST)
-http://192.168.0.X/remote/controller/lightStatus?ctrl=on&redvalue=200&greenvalue=200&bluevalue=200
-*/
-async function requestModuleControll(inst, opts){  
-    return new Promise(function(resolve, reject){
-        const uri = 'http://localhost:8080/remote/controller/' + inst;  //모듈쪽 주소로 변경
-
-    if(inst == 'lightStatus'){
+async function requestModuleController(inst, opts){
+    console.log(opts);
+    return new Promise((resolve, reject) => {
         request.post({
-            uri: uri,
-            qs:{
-                ctrl: opts.ctrl,
-                redValue: opts.redValue,
-                greenValue: opts.greenValue,
-                blueValue: opts.blueValue
-            },
+            uri: 'http://localhost:8080/remote/'+inst,
+            body:opts,
+            json: true
         },
-        function(error, httpResponse, body){
+        (error, httpResponse, body) => {
             if(!error && httpResponse.statusCode == 200){
-                resolve(true);
+                resolve(body);
+                console.log(body);
             }else{
-                reject(err);
+                reject(error);
             }
         }
         )
-    }
-    else if(inst = 'curtainStatus'){
-        request.post({
-            uri: uri,
-            qs:{
-                ctrl: opts.ctrl,
-                onTime: opts.onTime,
-                offTime: opts.offTime
-            }
+    })
+}
+
+async function requestModuleGetStatus(inst){
+    return new Promise((resolve, reject) => {
+        request({
+            uri: 'http://localhost:8080/remote/' + inst
         },
-        function(error, httpResponse, body){
-            if(!error && httpResponse.statusCode == 200){
-                resolve(true);
+        (error, httpResponse, body) => {
+            if(httpResponse.statusCode == 200){
+                resolve(body);    
             }else{
-                reject(err);
+                reject(error);
             }
         })
-    }else if(inst == 'tempStatus'){
-        request.post({
-            uri: uri,
-            qs: {
-                ctrl: opts.ctrl,
-                onTime: opts.onTime,
-                offTime: opts.offTime,
-                temp: opts.temp
-            }
-        },function(error, httpResponse, body){
-            if(!error && httpResponse.statusCode == 200){
-                resolve(true);
-            }else{
-                reject(err);
-            }
-        })
-    }
-    })    
+    })
 }
--- router/remote.js
+++ router/remote.js
@@ -4,38 +4,16 @@ import * as remoteController from '../controller/remote.js';
 
 
 const router = express.Router();
-/*
+
 router.get('/', remoteController.getStatus);
-/////////////////////////////
-//  tempremote controller  //
-//  Get status
-router.get('/tempcontroller' , remoteController.tempGetStatus);
-//  Update stauts
-router.post('/tempcontroller' , remoteController.tempControl);
-/////////////////////////////
 
-/////////////////////////////
-//  light controller       //
-//  Get status
 router.get('/lightcontroller' , remoteController.lightGetStatus);
-//  Update stauts
 router.post('/lightcontroller' , remoteController.lightControl);
-/////////////////////////////
 
-/////////////////////////////
-//curtain controller       //
-//  Get status
 router.get('/curtaincontroller' , remoteController.curtainGetStatus);
-//  Update stauts
 router.post('/curtaincontroller' , remoteController.curtainControl);
-/////////////////////////////
 
-*/
-// test api //
-
-router.get('/status/:inst', remoteController.testGetStatus);
-
-router.post('/controller/:inst', remoteController.testController)
-
+router.get('/tempcontroller' , remoteController.tempGetStatus);
+router.post('/tempcontroller' , remoteController.tempControl);
 
 export default router;
